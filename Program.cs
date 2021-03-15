a0106dd2e88f053eca8eafee53d1089bdfc644f9
Sebastiaan Booman <iMeowTwitch@gmail.com>
2021-03-15 09:18:20 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TheFatDuckRestaurant
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
