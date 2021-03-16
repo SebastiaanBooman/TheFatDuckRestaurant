ccc2102c07b478124fa796f254091a1c05e998a6
Sebastiaan Booman <iMeowTwitch@gmail.com>
2021-03-16 13:18:04 +0100

    Changed Sebastiaan Branch

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace TheFatDuckRestaurant
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("This is the Sebastiaan Branch!");
+            Console.WriteLine("Sebastiaan test change!");
         }
     }
 }
