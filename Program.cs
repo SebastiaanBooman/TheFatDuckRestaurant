1d614e4d45f115e4f515bd7422046bbb57fef805
Sebastiaan Booman <iMeowTwitch@gmail.com>
2021-03-22 22:58:48 +0100

    While loop toegevoegd voor de input bij de MenuGerechten Functie. showItem functie verbeterd en geoptimaliseerd.

--- Program.cs
+++ Program.cs
@@ -37,23 +37,44 @@ namespace TheFatDuckRestaurant
 
             Console.Clear();
             Console.WriteLine($"Dit zijn de {typeGerechtNaam} van The Fat Duck.\x0A\x0A");
-                for (int i = 1; i < typeGerecht.Length + 1; i++)
+            for (int i = 1; i < typeGerecht.Length + 1; i++)
+             {
+
+                 Console.WriteLine(typeGerecht[i - 1].naam);
+                 Console.WriteLine($"Toets {i} voor meer informatie over dit gerecht \x0A\x0A");
+             }
+            Console.WriteLine($"Toets Q om terug te gaan \x0A\x0A");
+            var a = Console.ReadLine();
+            int b = Int32.Parse(a);
+            bool passed = false;
+            while (!passed) // checkt of de user input wel op het menu staat of Q is, anders vraagt het om een nieuwe input.
                 {
-
-                    Console.WriteLine(typeGerecht[i - 1].naam);
-                    Console.WriteLine($"Toets {i} voor meer informatie over dit gerecht \x0A\x0A");
-
+                    if (b > typeGerecht.Length || b <= 0 || a != "Q") //TODO: System crashes als de input een string als "Q" is omdat hij een cijver verwacht bij line 48.
+                        {
+                        Console.WriteLine("Dit gerecht bestaat niet! Probeer een ander gerecht");
+                        a = Console.ReadLine();
+                        b = Int32.Parse(a);
                 }
-                Console.WriteLine($"Toets Q om terug te gaan \x0A\x0A");
+                    else
+                        {
+                    passed = true;
+                        }
+            }
+            showItem(b, typeGerecht);
         }
         public static void showItem(int x, Gerechten[] typeGerecht)
         {
             Console.Clear();
             Console.WriteLine($"Gerecht: " + typeGerecht[x - 1].naam + "\x0A\x0A");
-            Console.WriteLine($"Prijs: " + typeGerecht[x-1].prijs);
-            Console.WriteLine($"Beschrijving: " + typeGerecht[x - 1].beschrijving);
-            Console.WriteLine($"Ingredienten: " + typeGerecht[x - 1].ingredienten);
+            Console.WriteLine($"Prijs: " + typeGerecht[x-1].prijs + "\x0a");
+            Console.WriteLine($"Beschrijving: " + typeGerecht[x - 1].beschrijving + "\x0a");
+            Console.WriteLine($"Ingredienten: ");
+            for (int i = 0; i < typeGerecht[x-1].ingredienten.Length; i++)
+            {
+                Console.WriteLine(typeGerecht[x - 1].ingredienten[i]);
+            }
 
+            Console.WriteLine($"\x0a\x0aToets Q om terug te gaan");
         }
     }
 }
