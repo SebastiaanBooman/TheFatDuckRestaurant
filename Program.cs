bf9884f99ed5469b90ae948b871a182a4c842043
TomSchreur <74595520+TomSchreur@users.noreply.github.com>
2021-03-24 16:00:43 +0100

    clear

--- Program.cs
+++ Program.cs
@@ -1,12 +1,1 @@
-﻿using System;
-
-namespace TheFatDuckRestaurant
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("This is the Tom Branch!");
-        }
-    }
-}
+﻿
