3c3c20bceba058ae5b7f3dad6a570c8692934f4d
TomSchreur <74595520+TomSchreur@users.noreply.github.com>
2021-03-24 20:24:20 +0100

    inloggen verbeterd

--- Program.cs
+++ Program.cs
@@ -27,41 +27,38 @@ namespace TheFatDuckRestaurant
         {
             var jsonString = File.ReadAllText("gebruikers.json");
             Gebruikers gebruikers = JsonSerializer.Deserialize<Gebruikers>(jsonString);
-            Inlogscherm(gebruikers.Klanten, false);
+            while (true)
+            {
+                Inlogscherm(gebruikers.Klanten, false);
+            }
+        }
+
+        public static bool CheckWachtwoord(int index, Inloggen[] klant)
+        {
+            string GegevenWachtwoord = Console.ReadLine();
+            return GegevenWachtwoord == klant[index].Wachtwoord;
         }
         public static void Inlogscherm(Inloggen[] klant, bool Recursive)
         {
 
-            if (!Recursive)
-            {
-                Console.Clear();
-                Console.WriteLine($"Voer uw gebruikersnaam in.");
-            }
             bool NaamBestaat = Recursive;
+            Console.Clear();
+            Console.WriteLine($"Voer uw gebruikersnaam in.");
             string GegevenNaam = Console.ReadLine();
-            for (int i = 0; i < klant.Length && !NaamBestaat; i++)
+            int index = 0;
+            for (int i = 0; i < klant.Length && !NaamBestaat; i++) //checkt of de gebruikersnaam al bestaat
             {
-                if (GegevenNaam == klant[i].Gebruikersnaam) { NaamBestaat = true; }
+                if (GegevenNaam == klant[i].Gebruikersnaam) { NaamBestaat = true; index = i; }
             }
-            if (NaamBestaat)
+            if (NaamBestaat || Recursive)
             {
                 Console.WriteLine($"\x0AVoer uw wachtwoord in.");
-                bool WachtwoordBestaat = false;
-                string GegevenWachtwoord = Console.ReadLine();
-                for (int i = 0; i < klant.Length; i++)
+                while (!CheckWachtwoord(index, klant))
                 {
-                    if (GegevenNaam == klant[i].Wachtwoord) { WachtwoordBestaat = true; }
+                    Console.WriteLine("Verkeerd wachtwoord. Probeer het opnieuw.");
                 }
-                if (WachtwoordBestaat)
-                {
-                    Console.WriteLine($"\x0AU bent ingelogd!");
-                }
-                else 
-                { 
-                    Console.WriteLine("Verkeerd wachtwoord. Druk op Enter om het opnieuw te proberen.");
-                    Console.ReadLine();
-                    Inlogscherm(klant, true);
-                }
+                Console.WriteLine($"\x0AU bent ingelogd! Druk op Enter om verder te gaan.");
+                Console.ReadLine();
             }
             else 
             { 
@@ -72,40 +69,3 @@ namespace TheFatDuckRestaurant
         }
     }
 }
-
-            /*Console.WriteLine($"Toets Q om terug te gaan \x0A\x0A");
-            var a = Console.ReadLine();
-            int b = Int32.Parse(a);
-            bool passed = false;
-            while (!passed) // checkt of de user input wel op het menu staat of Q is, anders vraagt het om een nieuwe input.
-                {
-                    if (b > typeGerecht.Length || b <= 0 || a != "Q") //TODO: System crashes als de input een string als "Q" is omdat hij een cijver verwacht bij line 48.
-                        {
-                        Console.WriteLine("Dit gerecht bestaat niet! Probeer een ander gerecht");
-                        a = Console.ReadLine();
-                        b = Int32.Parse(a);
-                }
-                    else
-                        {
-                    passed = true;
-                        }
-            }
-            showItem(b, typeGerecht);
-        }
-        public static void showItem(int x, Gerechten[] typeGerecht)
-        {
-            Console.Clear();
-            Console.WriteLine($"Gerecht: " + typeGerecht[x - 1].naam + "\x0A\x0A");
-            Console.WriteLine($"Prijs: " + typeGerecht[x-1].prijs + "\x0a");
-            Console.WriteLine($"Beschrijving: " + typeGerecht[x - 1].beschrijving + "\x0a");
-            Console.WriteLine($"Ingredienten: ");
-            for (int i = 0; i < typeGerecht[x-1].ingredienten.Length; i++)
-            {
-                Console.WriteLine(typeGerecht[x - 1].ingredienten[i]);
-            }
-
-            Console.WriteLine($"\x0a\x0aToets Q om terug te gaan");
-        }
-    }
-}
-*/
