505b7a4f99787ceae8860ed992f215ed278aad73
Sebastiaan Booman <iMeowTwitch@gmail.com>
2021-03-31 15:37:22 +0200

    createItemMenu method gemaakt met functionaliteit om een menu item te creeeren en aan te passen.

--- Program.cs
+++ Program.cs
@@ -28,8 +28,8 @@ namespace TheFatDuckRestaurant
         static void Main(string[] args)
         {
             Menu menu = instantiateMenu();
-          //  addItemMenu(menu.Voorgerechten);
-            KiesMenu(menu);
+            addItemMenu(menu.Voorgerechten);
+            //KiesMenu(menu);
         }
         public static void KiesMenu(Menu menu)
         {
@@ -159,12 +159,7 @@ namespace TheFatDuckRestaurant
             {
                 WriteIndented = true,
             };
-            var newGerecht = new Gerechten { 
-                naam = "Kaas",
-                prijs = 10,
-                beschrijving = "Cheese",
-                ingredienten = new[] {"zuivel", "Kase" }
-            };
+            var newGerecht = createItemMenu();
             Menu menu = instantiateMenu();
 
             var newGerechten = new Gerechten[typeGerecht.Length +1];
@@ -181,6 +176,173 @@ namespace TheFatDuckRestaurant
             return menu;
 
         }
+        public static Gerechten createItemMenu()
+        {
+            var passed = false;
+
+            var naam_ = "<Nog geen naam>";
+            var prijs_ = 0.0;
+            var beschrijving_ = "<Nog geen beschrijving>";
+            string[] ingredienten_ = null; ;
+
+            while (!passed)
+            {
+                var passedSpecifiek = false;
+                Console.Clear();
+                Console.WriteLine("Voeg een nieuw item toe aan het menu \x0A");
+                Console.WriteLine($"Dit is de naam van het nieuwe item: \x0A{naam_} \x0AOm de naam aan te passen toets 1 en klik op enter\xA0");
+                Console.WriteLine($"\nDit is de prijs van het nieuwe item: \x0A{prijs_} \x0AOm de prijs aan te passen toets 2 en klik op enter");
+                Console.WriteLine($"\nDit is de beschrijving van het nieuwe item: \x0A{beschrijving_} \x0AOm de bescrhijving aan te passen toets 3 en klik op enter");
+                Console.WriteLine($"\nDit zijn de ingredienten van het nieuwe item: ");
+                if(ingredienten_ != null)
+                {
+                    for (int i = 0; i < ingredienten_.Length; i++)
+                    {
+                        Console.WriteLine(ingredienten_[i]);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("<Nog geen ingredienten>");
+                }
+                Console.WriteLine("Om de ingredienten aan te passen toets 4 en klik op enter");
+                Console.WriteLine($"\nOm het item te confirmeren en toe te voegen klik op 5\xA0");
+                Console.WriteLine($"Om te stoppen met het item te maken klik op 6");
+                var userInput = Console.ReadLine();
+                switch (userInput)
+                {
+                    case "1":
+                        Console.Clear();
+                        Console.WriteLine($"Naam aanpassen\nDit is de oude naam: {naam_}\x0A\x0AToets de nieuwe naam in en klik op enter\x0AOm terug te gaan toets Q in en klik op enter");
+                        var userInputNaam = Console.ReadLine();
+                        if(userInputNaam == "Q" || userInputNaam == "q") {}
+                        else
+                        {
+                            naam_ = userInputNaam;
+                        }
+                        break;
+                    case "2":
+                        Console.Clear();
+                        while(!passedSpecifiek){
+                            Console.WriteLine($"Prijs aanpassen\x0A Dit is de huidige prijs: {prijs_}\x0AToets de nieuwe prijs in en klik op enter\x0A Om terug te gaan toets Q in en klik op enter");
+                            var userInputPrijs = Console.ReadLine();
+                            if (userInputPrijs == "Q" || userInputPrijs == "q") 
+                            {
+                                passedSpecifiek = true;
+                            }
+                            else
+                            {
+
+                                try
+                                {
+                                    var userInputPrijsConverted = Int32.Parse(userInputPrijs);
+
+                                    prijs_ = userInputPrijsConverted;
+                                    passedSpecifiek = true;
+                                }
+                                catch (System.FormatException)
+                                {
+                                    try
+                                    {
+                                        var userInputPrijsConverted = float.Parse(userInputPrijs);
+                                        prijs_ = userInputPrijsConverted;
+                                        passedSpecifiek = true;
+                                    }
+                                    catch (System.FormatException)
+                                    {
+                                        Console.WriteLine("Verkeerde input, voer een int in.");
+                                    }
+                                    
+                                }
+                            }
+                        }
+                        passedSpecifiek = true;
+                        break;
+                    case "3":
+                        Console.Clear();
+                        Console.WriteLine($"Beschrijving aanpassen\x0A Dit is de huidige beschrijving: {beschrijving_}\x0AToets de nieuwe beschrijving in en klik op enter\x0A Om terug te gaan toets Q in en klik op enter.");
+                        var userInputBeschrijving = Console.ReadLine();
+                        if(userInputBeschrijving == "Q" || userInputBeschrijving == "q") {}
+                        else
+                        {
+                            beschrijving_ = userInputBeschrijving;
+                        }
+                        break;
+                    case "4":
+                        Console.Clear();
+                        while (!passedSpecifiek)
+                        {
+                            Console.Clear();
+                            Console.WriteLine($"Ingredienten aanpassen\x0A Dit zijn de huidige ingredienten:");
+                            if(ingredienten_ != null)
+                            {
+                                for(int i = 0; i < ingredienten_.Length; i++)
+                                {
+                                    Console.WriteLine(ingredienten_[i]);
+                                }
+                            }
+                            else
+                            {
+                                Console.WriteLine("<Nog geen ingredienten.>");
+                            }
+                            Console.WriteLine("\x0AToets een nieuw ingredient in en klik op enter\x0A Om terug te gaan toets Q en klik op enter.");
+                            var userInputIngredienten = Console.ReadLine();
+                            if (userInputIngredienten == "Q" || userInputIngredienten == "q") 
+                            {
+                                passedSpecifiek = true;
+                            }
+                            else
+                            {
+                                if(ingredienten_ != null)
+                                {
+                                    var tempIngredienten = new string[ingredienten_.Length +1];
+                                    for(int i = 0; i < ingredienten_.Length; i++)
+                                    {
+                                        tempIngredienten[i] = ingredienten_[i];
+                                    }
+                                    tempIngredienten[tempIngredienten.Length - 1] = userInputIngredienten;
+                                    ingredienten_ = tempIngredienten;
+                                }
+                                else
+                                {
+                                    ingredienten_ = new string[1];
+                                    ingredienten_[0] = userInputIngredienten;
+                                }
+                            }
+                        }
+                        break;
+                    case "5":
+                        Console.Clear();
+                        Console.WriteLine("Het item wordt toegevoegd aan het voorgerechten menu, weet u dit zeker? \x0AToets A in en klik op enter om het menu toe te voegen. \x0AToets Q in en klik op enter om het te annuleren.");
+                        var userInputConfirmatie = Console.ReadLine();
+                        if(userInputConfirmatie == "A" || userInputConfirmatie == "a")
+                        {
+                            passed = true;
+                        }
+                        break;
+                    case "6":
+                        Console.Clear();
+                        Console.WriteLine("U gaat terug naar het algemene menu en er worden verder geen gerechten toegevoegd aan het menu, weet u dit zeker? \x0AToets A in en klik op enter om terug te gaan\x0A Toets Q in en klik op enter om verder te werken aan een menu item toevoegen. ");
+                        var userInputFinale = Console.ReadLine();
+                        if(userInputFinale == "A" || userInputFinale == "a")
+                        {
+                            //Call naar menu bekijken voor Werknemer scherm.
+                        }
+                        break;
+                }
+            }
+
+            var newGerecht = new Gerechten
+            {
+                naam = naam_,
+                prijs = prijs_,
+                beschrijving = beschrijving_,
+                ingredienten = ingredienten_
+            };
+
+            return newGerecht;
+        }
+
         public static Menu instantiateMenu()
         {
             var jsonString = File.ReadAllText("menu.json");
