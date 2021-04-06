33b1493a6b29dcbbdbb7c4d9f8880b2807af97c1
TomSchreur <74595520+TomSchreur@users.noreply.github.com>
2021-04-06 19:54:43 +0200

    .

--- Program.cs
+++ Program.cs
@@ -47,6 +47,7 @@ namespace TheFatDuckRestaurant
         public static void Inlogscherm(Inloggen[] gebruiker)
         {
             Func<int, Inloggen[], Tuple<bool, string>> CheckWachtwoord = (index, gebruiker) => { string Input = Console.ReadLine(); return Tuple.Create(Input == gebruiker[index].Wachtwoord || Input == "terug", Input); };
+            //returnt een tuple die aangeeft of de input het juiste wachtwoord of 'terug' is en de input als een string
             bool NaamBestaat = false;
             Console.Clear();
             Console.WriteLine($"Voer uw gebruikersnaam in.");
@@ -60,18 +61,18 @@ namespace TheFatDuckRestaurant
             {
                 Console.WriteLine($"\x0AVoer uw wachtwoord in.");
                 Tuple<bool, string> Password = CheckWachtwoord(index, gebruiker);
-                while (!Password.Item1) //blijft om het wachtwoord vragen totdat het juiste wachtwoord voor de gebruikersnaam wordt gegeven
+                while (!Password.Item1) //blijft om het wachtwoord vragen totdat het juiste wachtwoord voor de gebruikersnaam wordt gegeven of er 'terug' wordt getypt
                 {
                     Console.WriteLine("Verkeerd wachtwoord. Probeer het opnieuw of type 'terug' om terug te gaan.");
                     Password = CheckWachtwoord(index, gebruiker);
                 }
-                if (Password.Item2 != "terug")
+                if (Password.Item2 != "terug") //sluit het inlogscherm af wanneer 'terug' was getypt
                 {
                     Console.WriteLine($"\x0AU bent ingelogd! Druk op Enter om verder te gaan.");
                     Console.ReadLine();
                 }
             }
-            else //reset het inlogscherm wanneer een nog niet geregistreerde gebruikersnaam wordt gegeven
+            else //reset het inlogscherm wanneer een nog niet geregistreerde gebruikersnaam wordt gegeven of sluit het inlogscherm af wanneer 'terug' is getypt
             { 
                 Console.WriteLine("Verkeerde gebruikersnaam. Druk op Enter om het opnieuw te proberen of type 'terug' om terug te gaan.");
                 if (Console.ReadLine() != "terug")
