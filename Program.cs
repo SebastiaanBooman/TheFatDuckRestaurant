530f47a241944a48c21121cc898a071e605ae583
TomSchreur <74595520+TomSchreur@users.noreply.github.com>
2021-04-09 13:54:57 +0200

    begin reservatie

--- Program.cs
+++ Program.cs
@@ -17,34 +17,49 @@ namespace TheFatDuckRestaurant
     {
         public string Gebruikersnaam { get; set; }
         public string Wachtwoord { get; set; }
+        public Reservatie Reservatie { get; set; }
     }
-
+    public class Reservatie
+    {
+        public int Tijd { get; set; }
+        public int Dag { get; set; }
+        public string Maand { get; set; }
+        public int Personenen { get; set; }
+    }
 
 
     public class Program
     {
         static void Main(string[] args)
         {
+            var jsonString = File.ReadAllText("gebruikers.json");
+            Gebruikers gebruikers = JsonSerializer.Deserialize<Gebruikers>(jsonString);
+            Dictionary<string,dynamic> HuidigeGebruiker = null;
             while (true)
             {
-                var jsonString = File.ReadAllText("gebruikers.json");
-                Gebruikers gebruikers = JsonSerializer.Deserialize<Gebruikers>(jsonString);
                 Console.Clear();
+                if (HuidigeGebruiker != null) 
+                { 
+                    Console.WriteLine(HuidigeGebruiker["Medewerker"]);
+                    Console.WriteLine(HuidigeGebruiker["Gebruiker"].Reservatie.Maand);
+                    Reserveren(HuidigeGebruiker["Gebruiker"]);
+                    Console.WriteLine(HuidigeGebruiker["Gebruiker"].Reservatie.Maand);
+                }
                 Console.WriteLine("Wilt u als klant ('a') of als medewerker ('b') inloggen?");
                 string Choice = Console.ReadLine();
                 if(Choice == "a")
                 {
-                    Inlogscherm(gebruikers.Klanten);
+                    HuidigeGebruiker = Inlogscherm(gebruikers.Klanten,gebruikers);
                 }
                 else if(Choice == "b")
                 {
-                    Inlogscherm(gebruikers.Medewerkers);
+                    HuidigeGebruiker = Inlogscherm(gebruikers.Medewerkers,gebruikers);
                 }
             }
         }
         
 
-        public static void Inlogscherm(Inloggen[] gebruiker)
+        public static Dictionary<string,dynamic> Inlogscherm(Inloggen[] gebruiker, Gebruikers gebruikers)
         {
             Func<int, Inloggen[], Tuple<bool, string>> CheckWachtwoord = (index, gebruiker) => { string Input = Console.ReadLine(); return Tuple.Create(Input == gebruiker[index].Wachtwoord || Input == "terug", Input); };
             //returnt een tuple die aangeeft of de input het juiste wachtwoord of 'terug' is en de input als een string
@@ -72,6 +87,11 @@ namespace TheFatDuckRestaurant
                     {
                         Console.WriteLine($"\x0AU bent ingelogd! Druk op Enter om verder te gaan.");
                         Console.ReadLine();
+                        Dictionary<string, dynamic> dic = new Dictionary<string, dynamic>();
+                        dic.Add("Gebruiker", gebruiker[index]);
+                        dic.Add("Medewerker", gebruiker == gebruikers.Medewerkers);
+                        return dic;
+                        //return Tuple.Create(gebruiker, index);
                     }
                 }
                 else //reset het inlogscherm wanneer een nog niet geregistreerde gebruikersnaam wordt gegeven of sluit het inlogscherm af wanneer 'terug' is getypt
@@ -79,10 +99,15 @@ namespace TheFatDuckRestaurant
                     Console.WriteLine("Verkeerde gebruikersnaam. Druk op Enter om het opnieuw te proberen of type 'terug' om terug te gaan.");
                     if (Console.ReadLine() != "terug")
                     {
-                        Inlogscherm(gebruiker);
+                        return Inlogscherm(gebruiker, gebruikers);
                     }
                 }
             }
+            return null;
+        }
+        public static void Reserveren(Inloggen klant)
+        {
+            klant.Reservatie.Maand = "Februari";
         }
     }
 }
