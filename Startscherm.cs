d43b714500975eeae211a2fc63b221598d24c04d
Sebastiaan Booman <iMeowTwitch@gmail.com>
2021-04-24 16:54:00 +0200

    Added ASCIIArt + Changed Startscherm optie menu voor duidelijkheid

--- Startscherm.cs
+++ Startscherm.cs
@@ -15,6 +15,32 @@ namespace TheFatDuckRestaurant
             //KiesMenu(menu);
             StartschermFunctie();
         }
+
+
+        public static string ASCIIART()
+        {
+            string ASCIILogoL1 = "  _______ _            ______    _     _____             _     ";
+            string ASCIILogoL2 = " |__   __| |          |  ____|  | |   |  __ \\           | |    ";
+            string ASCIILogoL3 = "    | |  | |__   ___  | |__ __ _| |_  | |  | |_   _  ___| | __ ";
+            string ASCIILogoL4 = "    | |  | '_ \\ / _ \\ |  __/ _` | __| | |  | | | | |/ __| |/ / ";
+            string ASCIILogoL5 = "    | |  | | | |  __/ | | | (_| | |_  | |__| | |_| | (__|   <  ";
+            string ASCIILogoL6 = "    |_|  |_| |_|\\___| |_|  \\__,_|\\__| |_____/ \\__,_|\\___|_|\\_\\ ";
+
+            string ASCIIDuckL1 = "           ..\n";
+            string ASCIIDuckL2 = "          ( '`<\n";
+            string ASCIIDuckL3 = "           )(\n";
+            string ASCIIDuckL4 = "    ( ----'  '.\n";
+            string ASCIIDuckL5 = "    (         ;\n";
+            string ASCIIDuckL6 = "     (_______,'\n";
+
+
+            string ASCIILogoAll = ASCIILogoL1 + ASCIIDuckL1 + ASCIILogoL2 + ASCIIDuckL2 + ASCIILogoL3 + ASCIIDuckL3 + ASCIILogoL4 + ASCIIDuckL4 + ASCIILogoL5 + ASCIIDuckL5 + ASCIILogoL6 + ASCIIDuckL6;
+
+
+            Console.WriteLine(ASCIILogoAll);
+            return ASCIILogoAll;
+
+        }
         public static void StartschermFunctie()
         {
             bool verkeerdeInput = false;
@@ -22,10 +48,11 @@ namespace TheFatDuckRestaurant
             while (!passed)
             {
                 Console.Clear();
-                Console.WriteLine("The Fat Duck Restaurant\x0aToets op 1 om meer informatie te zien over The Fat Duck\x0a");
-                Console.WriteLine("Inloggen\x0aToets op 2 om in te loggen bij The Fat Duck\x0a");
-                Console.WriteLine("Menu Bekijken\x0aKlik op 3 om het menu te bezichtigen\x0a");
-                Console.WriteLine("Afsluiten\x0aKlik op 4 om de applicatie te sluiten");
+                ASCIIART();
+                Console.WriteLine("1: Informatie over The Fat Duck\x0a");
+                Console.WriteLine("2: Login bij The Fat Duck\x0a");
+                Console.WriteLine("3: Bezichtig het menu\x0a");
+                Console.WriteLine("4: Applicatie afsluiten");
 
                 if (verkeerdeInput)
                     Console.WriteLine("Verkeerde input, probeer 1,2,3 of 4");
@@ -61,11 +88,12 @@ namespace TheFatDuckRestaurant
             while (!passed)
             {
                 Console.Clear();
+                ASCIIART();
                 Console.WriteLine("Informatie over Fat Duck restaurant\n\n");
                 Console.WriteLine("<Informatie over the Fat Duck>\n\n");
-                Console.WriteLine("\x0aKlik op 1 om terug naar het startscherm te gaan");
+                Console.WriteLine("1: Terug naar startscherm");
                 if (verkeerdeInput)
-                    Console.WriteLine("VerkeerdeInput, probeer q");
+                    Console.WriteLine("VerkeerdeInput, probeer 1");
 
                 ConsoleKeyInfo userInput = Console.ReadKey();
                 char userInputChar = userInput.KeyChar;
