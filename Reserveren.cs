e886483f759e42b3559f4afa6532ddbfee9f8c3f
TomSchreur <74595520+TomSchreur@users.noreply.github.com>
2021-04-26 10:49:19 +0200

    Reserveren zonder json

--- Reserveren.cs
+++ Reserveren.cs
@@ -23,14 +23,45 @@ namespace TheFatDuckRestaurant
             {
                 Console.WriteLine($"{i}: Reserveer voor {Dagen[i].Item1}/{Dagen[i].Item2}/{Dagen[i].Item3}\x0a");
             }
+            if (int.TryParse(Console.ReadLine(), out int Keuze)){
+                Console.Clear();
+                Console.WriteLine($"{Dagen[Keuze].Item1}/{Dagen[Keuze].Item2}/{Dagen[Keuze].Item3}\x0a");
+                string[] Beschikbaar = Tijden(Dagen[Keuze]);
+                for(int i = 0; i < Beschikbaar.Length; i++)
+                {
+                    Console.WriteLine($"{i}: Reserveer voor {Beschikbaar[i]} ({Plaatsen(Dagen[Keuze],Beschikbaar[i])} vrije plekken)\x0a");
+                }
+                if (int.TryParse(Console.ReadLine(), out int Keuze2))
+                {
+                    Console.Clear();
+                    Console.WriteLine($"U heeft gereserveerd voor: {Dagen[Keuze].Item1}/{Dagen[Keuze].Item2}/{Dagen[Keuze].Item3} om {Beschikbaar[Keuze2]}\x0a\x0a"+"Enter: Ga terug naar het beginscherm");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Verkeerde input\x0a"+"Enter: Ga terug naar het beginscherm");
+            }
             Console.ReadKey();
         }
 
+        public static int Plaatsen(Tuple<int,int,int> Datum, string Tijd)
+        {
+            return 100;
+        }
+        public static string[] Tijden(Tuple<int,int,int> Datum)
+        {
+            string[] Tijdarr = new string[11];
+            for(int i = 0, j = 11;i < 11; i++,j++)
+            {
+                Tijdarr[i] = $"{j}:00";
+            }
+            return Tijdarr;
+        }
         public static Tuple<int, int, int>[] Datums(DateTime Date)
         {
             int Day = Date.Day, Month = Date.Month, Year = Date.Year;
-            Tuple<int, int, int>[] tarr = new Tuple<int, int, int>[14];
-            tarr[0] = Tuple.Create(Day, Month, Year);
+            Tuple<int, int, int>[] Dagen = new Tuple<int, int, int>[14];
+            Dagen[0] = Tuple.Create(Day, Month, Year);
             for (int i = 1; i < 14; i++)
             {
                 Day++;
@@ -74,9 +105,9 @@ namespace TheFatDuckRestaurant
                         Month++;
                     }
                 }
-                tarr[i] = Tuple.Create(Day, Month, Year);
+                Dagen[i] = Tuple.Create(Day, Month, Year);
             }
-            return tarr;
+            return Dagen;
         }
     }
 }
