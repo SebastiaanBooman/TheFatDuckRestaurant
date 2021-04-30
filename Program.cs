0b59136b48389b57c186740e80ae06736c2a04e9
Rafael Januszewski <0993044@hr.nl>
2021-04-30 10:17:33 +0200

    Registration system no newtonsoft

--- Program.cs
+++ Program.cs
@@ -4,171 +4,194 @@ using System.IO;
 using System.Text.Json;
 using System.Text.Json.Serialization;
 using TheFatDuckRestaurant;
-using Newtonsoft.Json;
 
 namespace TheFatDuckRestaurant
 {
-    public class Werknemers
+    public class Klanten
     {
         public string Naam { get; set; }
         public string Wachtwoord { get; set; }
-        public Werknemers(string naam, string wachtwoord)
-        {
-            Naam = naam;
-            Wachtwoord = wachtwoord;
-        }
-
     }
 
-}
-
-class Program
-{
-    static void Main(string[] args)
+    public class Medewerker
     {
-        var arrUsers = new Users[]
+        public string Naam { get; set; }
+        public string Wachtwoord { get; set; }
+    }
+
+    public class Gebruikers
+    {
+        public List<Klanten> Klanten { get; set; }
+        public List<Medewerker> Medewerkers { get; set; }
+    }
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
         {
+            var arrUsers = new Users[]
+            {
             new Users("Tom","test"),
             new Users("Sebastian","test"),
-        };
-        bool successfull = false;
+            };
+            bool successfull = false;
 
 
-        void login()
-        {
-            Console.WriteLine("Enter your username:");
-            var username = Console.ReadLine();
-            Console.Clear();
-            Console.WriteLine("Enter your password:");
-            var password = Console.ReadLine();
-            Console.Clear();
-
-
-            foreach (Users user in arrUsers)
+            void login()
             {
-                if (username == user.username && password == user.password)
+                Console.WriteLine("Enter your username:");
+                var username = Console.ReadLine();
+                Console.Clear();
+                Console.WriteLine("Enter your password:");
+                var password = Console.ReadLine();
+                Console.Clear();
+
+
+                foreach (Users user in arrUsers)
                 {
-                    Console.WriteLine("You have successfully logged in!");
-                    Console.ReadLine();
-                    successfull = true;
-                    break;
+                    if (username == user.username && password == user.password)
+                    {
+                        Console.WriteLine("You have successfully logged in!");
+                        Console.ReadLine();
+                        successfull = true;
+                        break;
+                    }
+                }
+
+                if (!successfull)
+                {
+                    Console.WriteLine("Your username or password is incorect, try again");
+                }
+
+
+            }
+            void register()
+            {
+                var jsonOptions = new JsonSerializerOptions
+                {
+                    WriteIndented = true,
+                };
+                    var user = File.ReadAllText(@"C:\Users\User\source\repos\SebastiaanBooman\TheFatDuckRestaurant\credentials.json");
+                    Gebruikers jsonDes = JsonSerializer.Deserialize<Gebruikers>(user);
+
+
+                    Console.WriteLine("press 1 to register klant or press 2 to register medewerker");
+                    var input = Console.ReadLine();
+                    Console.Clear();
+
+                    if (input == "1")
+                    {
+                        Console.WriteLine("Enter your username:");
+                        string username = Console.ReadLine();
+                        foreach (var item in jsonDes.Klanten)
+                        {
+                            while (item.Naam == username)
+                            {
+                                Console.Clear();
+                                Console.WriteLine("Username exists, enter different username");
+                                username = Console.ReadLine();
+                            }
+
+                        }
+                        Console.WriteLine("Enter your password:");
+                        string password = Console.ReadLine();
+                        Console.Clear();
+                        jsonDes.Klanten.Add(new Klanten()
+                            {
+                                Naam = username,
+                                Wachtwoord = password
+                            });
+                            var toSerializeKlant = JsonSerializer.Serialize(jsonDes, jsonOptions);
+                            File.WriteAllText(@"C:\Users\User\source\repos\SebastiaanBooman\TheFatDuckRestaurant\credentials.json", toSerializeKlant);
+
+
+                    }
+
+                    else if (input == "2")
+                    {
+                        Console.WriteLine("Enter your username:");
+                        string username = Console.ReadLine();
+                        foreach (var item in jsonDes.Medewerkers)
+                        {
+                            while (item.Naam == username)
+                            {
+                                Console.Clear();
+                                Console.WriteLine("Username exists, enter different username");
+                                username = Console.ReadLine();
+                            }
+
+                        }
+
+                            Console.WriteLine("Enter your password:");
+                            string password = Console.ReadLine();
+                            Console.Clear();
+                            jsonDes.Medewerkers.Add(new Medewerker()
+                            {
+                                Naam = username,
+                                Wachtwoord = password
+                            });
+                            var toSerializeMedewerker = JsonSerializer.Serialize(jsonDes, jsonOptions);
+                            File.WriteAllText(@"C:\Users\User\source\repos\SebastiaanBooman\TheFatDuckRestaurant\credentials.json", toSerializeMedewerker);
                 }
             }
 
-            if (!successfull)
-            {
-                Console.WriteLine("Your username or password is incorect, try again");
-            }
 
+
+
+
+                while (!successfull)
+                {
+                    Console.WriteLine("Welcome to The Fat Duck! \x0APress 1 to login with your credentials \x0APress 2 to register \x0APress 3 for more informations \x0APress 4 for menu");
+                    var input = Console.ReadLine();
+                    Console.Clear();
+
+                    if (input == "1")
+                    {
+                        login();
+
+                    }
+
+                    else if (input == "2")
+                    {
+
+                        register();
+
+                    }
+
+                    else if (input == "3")
+                    {
+                        Console.WriteLine("Here you can find more informations");
+
+                    }
+                    else if (input == "4")
+                    {
+                        Console.WriteLine("Menu should be here");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Try again!");
+
+
+                    }
+
+                }
 
         }
-        void register()
+
+        public class Users
         {
-            Console.WriteLine("Enter your username:");
-            string username = Console.ReadLine();
-
-            Console.WriteLine("Enter your password:");
-            string password = Console.ReadLine();
-            Console.Clear();
-
-            var user = File.ReadAllText(@"C:\Users\User\source\repos\SebastiaanBooman\TheFatDuckRestaurant\credentials.json");
-            //Werknemers Tempor = JsonConvert.DeserializeObject<Werknemers>(user);
-            //string test = JsonConvert.SerializeObject(Tempor, Formatting.Indented);
-            //Console.WriteLine(Tempor.Naam);
+            public string username;
+            public string password;
 
 
+            public Users(string username, string password)
+            {
+                this.username = username;
+                this.password = password;
 
-
-            var list = JsonConvert.DeserializeObject<List<Werknemers>>(user);
-            //var list = JsonSerializer.Deserialize<TheFatDuckRestaurant.Werknemers>(user);
-            list.Add(new Werknemers(username, password));
-            var convertedJson = JsonConvert.SerializeObject(list, Formatting.Indented);
-
-
-            Console.WriteLine(convertedJson);
-            File.WriteAllText(@"C:\Users\User\source\repos\SebastiaanBooman\TheFatDuckRestaurant\credentials.json", convertedJson);
-
-            //string tempor = JsonConvert.SerializeObject(newuser, Formatting.Indented);
-            //
-            //Console.WriteLine(tempor);
-
-
-
-
-
-            //Werknemers werknemer1 = new Werknemers(username, password);
-            //werknemer1 = JsonConvert.DeserializeObject<Werknemers>(user);
-            //string jsonString = JsonConvert.SerializeObject(werknemer1);
-
-
-
-
-
-
-
-
-
-
-
-            //Console.WriteLine(werknemers);
-
-
+            }
         }
-
-
-
-
-
-        while (!successfull)
-        {
-            Console.WriteLine("Welcome to The Fat Duck! \x0APress 1 to login with your credentials \x0APress 2 to register \x0APress 3 for more informations \x0APress 4 for menu");
-            var input = Console.ReadLine();
-            Console.Clear();
-
-            if (input == "1")
-            {
-                login();
-
-            }
-
-            else if (input == "2")
-            {
-
-                register();
-
-            }
-
-            else if (input == "3")
-            {
-                Console.WriteLine("Here you can find more informations");
-
-            }
-            else if (input == "4")
-            {
-                Console.WriteLine("Menu should be here");
-            }
-            else
-            {
-                Console.WriteLine("Try again!");
-
-
-            }
-
-        }
-
-    }
-}
-
-public class Users
-{
-    public string username;
-    public string password;
-
-
-    public Users(string username, string password)
-    {
-        this.username = username;
-        this.password = password;
-
     }
 }
