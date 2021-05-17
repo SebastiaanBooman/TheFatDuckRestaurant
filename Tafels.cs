e6fc8952de70a20693d195a9bb7646f5c655dae2
Sebastiaan Booman <iMeowTwitch@gmail.com>
2021-05-17 21:35:17 +0200

    Added Tafels.cs and Tafels.json voor tafel management

---
+++ Tafels.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TheFatDuckRestaurant
+{
+    public class TafelArray
+    {
+        public Tafel Tafels { get; set; }
+    }
+
+
+    public class Tafel
+    {
+        public string ID { get; set; }
+        public int Plekken { get; set; }
+        public bool Gereserveerd { get; set; }
+    }
+}
