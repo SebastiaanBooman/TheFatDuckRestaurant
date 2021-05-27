163b5387f96ea1b2ed5d9901243f0e4cf29c421a
Sebastiaan Booman <iMeowTwitch@gmail.com>
2021-05-27 14:18:27 +0200

    kleine verbeteringen voor Tafels koppelen als medewerker

--- Tafels.cs
+++ Tafels.cs
@@ -13,94 +13,75 @@ namespace TheFatDuckRestaurant
 
         public TafelArray() { } //Empty constructor for json deserializen
 
-           public void BekijkVrijeTafels(string tijdEnDatum) //Todo: tafels zijn ook niet vrij als ze een aantal uur voor de gegeven tijd bezet zijn.
-          {
+        public void BekijkVrijeTafels(string tijdEnDatum) //Todo: tafels zijn ook niet vrij als ze een aantal uur voor de gegeven tijd bezet zijn.
+        {
             int checkTijd = tijdEnDatumNaarInt(tijdEnDatum); //De gegeven tijd in int (1200)
             string checkDatum = tijdEnDatumNaarDatum(tijdEnDatum); //De gegeven datum in string (Zaterdag 1 januari 2021)
 
-               Console.Clear();
-               Console.WriteLine($"Vrije Tafels in The Fat Duck voor {tijdEnDatum} (+-2 uur extra voor lopende reserveringen)\n");
-               Console.WriteLine("ID:\tPlekken:\n");
-               foreach (Tafel tafel in Tafels)
-               {
+            Console.Clear();
+            Console.WriteLine($"Vrije Tafels in The Fat Duck voor {tijdEnDatum} (+-2 uur extra voor lopende reserveringen)\n");
+            Console.WriteLine("ID:\tPlekken:\n");
+            foreach (Tafel tafel in Tafels)
+            {
                 bool alGereserveerd = false;
                 if (tafel.Gereserveerd != null)
                 {
                     foreach (string data in tafel.Gereserveerd)
                     {
-                        //Console.WriteLine($"{tijdEnDatumNaarDatum(data)}, {checkDatum}");
-                        //Console.ReadLine();
-                        if (tijdEnDatumNaarDatum(data) == checkDatum) //Als het op dezelfde dag plaats vind
+                        if (isEenTafelAlGereserveerd(data, checkDatum, checkTijd))
                         {
-                            if(tijdCheck(checkTijd, tijdEnDatumNaarInt(data), 200)){ //checkt of de tafel al gereserveerd is ergens binnen 200 (2 uur) van tevoren of 2 uur daarna, zo ja, Is al gereserveerd.
-                                alGereserveerd = true;
-                                break;
-                            }
+                            alGereserveerd = true;
+                            break;
                         }
                     }
                 }
-                   if (alGereserveerd != true)
-                       Console.WriteLine($"{tafel.ID}, {tafel.Plekken}\n");
-               }
-           }
-           public int tijdEnDatumNaarInt(string tijdEnDatum)
-        {
-            int tijd = int.Parse(tijdEnDatum.Substring(0,4)); //pakt de eerste 4 characters van een tijd (1200)
-            return tijd;
-        }
-
-        public string tijdEnDatumNaarDatum(string tijdEnDatum)
-        {
-            string datum = tijdEnDatum.Substring(4); //pakt de characters na de tijd (datum)
-            return datum;
-        }
-
-        public bool tijdCheck(int tafelTijd, int reserveringTijd, int uren) 
-        //Als tafeltijd (1200) + uren (200) is groter dan reserveringtijd (1324), of als reserveringstijd (1324) - uren (200) is kleiner of gelijk aan tafeltijd (1200) returnt true, anders false.
-        {
-            Console.WriteLine($"Tafeltijd:{tafelTijd}, reserveringstijd: {reserveringTijd}, uren: {uren}");
-            Console.ReadLine();
-            if ((tafelTijd + uren >= reserveringTijd) || ((reserveringTijd - uren) <= tafelTijd))
-            {
-                //Console.WriteLine("Al gereserveerd");
-                //Console.ReadLine();
-                return true;
+                if (alGereserveerd != true)
+                    Console.WriteLine($"{tafel.ID}, {tafel.Plekken}\n");
             }
+        }
+        public int tijdEnDatumNaarInt(string tijdEnDatum) => int.Parse(tijdEnDatum.Substring(0,4)); //returnt de eerste 4 characters van een tijd (1200)
+        public string tijdEnDatumNaarDatum(string tijdEnDatum) => tijdEnDatum.Substring(4); //returnt de characters na de tijd (datum)
+        public bool tijdCheck(int tafelTijd, int reserveringTijd, int uren) => ((tafelTijd + uren >= reserveringTijd) || ((reserveringTijd - uren) <= tafelTijd)) ? true : false; //return true Als tafeltijd + uren groter is dan reserveringtijd, of als reserveringstijd - uren kleiner of gelijk is aan tafeltijd.
+
+        public int BerekenTafelsDieNogGekoppeldMoetenWorden(int aantalMensen, List<Tafel> gereserveerdeTafels)
+        {
+            int totaleGereserveerdePlekken = 0;
+            foreach (Tafel tafel in gereserveerdeTafels)
+            {
+                totaleGereserveerdePlekken += tafel.Plekken;
+                if (totaleGereserveerdePlekken >= aantalMensen)
+                    return -1; //returns negative int als er genoeg tafels zijn gekoppeld
+            }
+            return aantalMensen - totaleGereserveerdePlekken; // returns de hoeveelheid plekken die nog gekkopeled moeten worden.
+        }
+
+        private bool isEenTafelAlGereserveerd(string tafelData, string checkDatum, int checkTijd)
+        {
+            if ((tijdEnDatumNaarDatum(tafelData) == checkDatum) && (tijdCheck(checkTijd, tijdEnDatumNaarInt(tafelData), 200))) //Als het op dezelfde dag plaats vind dan checkt of de tafel al gereserveerd is ergens binnen 200 (2 uur) van tevoren of 2 uur daarna, zo ja, Is al gereserveerd.
+                return true;
             return false;
         }
-        
-           public int BerekenTafelsDieNogGekoppeldMoetenWorden(int aantalMensen, List<Tafel> gereserveerdeTafels)
-           {
-               int totaleGereserveerdePlekken = 0;
-               foreach(Tafel tafel in gereserveerdeTafels)
-               {
-                   totaleGereserveerdePlekken += tafel.Plekken;
-                   if (totaleGereserveerdePlekken >= aantalMensen)
-                       return -1; //returns negative int als er genoeg tafels zijn gekoppeld
-               }
-               return aantalMensen - totaleGereserveerdePlekken; // returns de hoeveelheid plekken die nog gekkopeled moeten worden.
-           }
 
-           public List<Tafel> AutomatischKoppelen(int aantalMensen, List<Tafel> gereserveerdeTafels, string tijdEnDatum, bool tochKoppelen = false) //TODO: Aantal plekken zou berekend kunnen worden (voor iedere tafel in de lijst die al beschikbaar is - aantalMensen) zodat er niet dubbel gekoppeld kan worden.
-           {
-               while (true)
-               {
-                   Console.Clear();
-                   Console.WriteLine(ASCIIART.TafelsArt());
-                   if (BerekenTafelsDieNogGekoppeldMoetenWorden(aantalMensen, gereserveerdeTafels) <= 0) //0 megenomen omdat dat aangeeft dat er precies genoeg tafels zijn gekoppeld.
-                   {
-                       Console.WriteLine("Er zijn al genoeg tafels gekoppeld aan deze reservering. Klik op een toets om terug te gaan");
-                       ConsoleKeyInfo input = Console.ReadKey();
-                       return gereserveerdeTafels;
-                   }
+        public List<Tafel> AutomatischKoppelen(int aantalMensen, List<Tafel> gereserveerdeTafels, string tijdEnDatum, bool tochKoppelen = false) //TODO: Aantal plekken zou berekend kunnen worden (voor iedere tafel in de lijst die al beschikbaar is - aantalMensen) zodat er niet dubbel gekoppeld kan worden.
+        {
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine(ASCIIART.TafelsArt());
+                if (BerekenTafelsDieNogGekoppeldMoetenWorden(aantalMensen, gereserveerdeTafels) <= 0) //0 megenomen omdat dat aangeeft dat er precies genoeg tafels zijn gekoppeld.
+                {
+                    Console.WriteLine("Er zijn al genoeg tafels gekoppeld aan deze reservering. Klik op een toets om terug te gaan");
+                    ConsoleKeyInfo input = Console.ReadKey();
+                    return gereserveerdeTafels;
+                }
 
-                   bool wrongInput = false;
-                   Console.WriteLine($"Er worden automatisch tafels toegevoegd die passen bij {aantalMensen} personen, weet u dit zeker?\n");
-                   Console.WriteLine($"1: Ja\n");
-                   Console.WriteLine($"0: Nee\n");
-                   if (wrongInput)
-                       Console.WriteLine("Wrong Input! Probeer 1 of 0");
-                   char userInput = Console.ReadKey().KeyChar;
+                bool wrongInput = false;
+                Console.WriteLine($"Er worden automatisch tafels toegevoegd die passen bij {aantalMensen} personen, weet u dit zeker?\n");
+                Console.WriteLine($"1: Ja\n");
+                Console.WriteLine($"0: Nee\n");
+                if (wrongInput)
+                    Console.WriteLine("Wrong Input! Probeer 1 of 0");
+                char userInput = Console.ReadKey().KeyChar;
                 switch (userInput)
                 {
                     case '1':
@@ -121,10 +102,10 @@ namespace TheFatDuckRestaurant
                                 bool geenGoedeTafel = false;
                                 foreach (string data in tafel.Gereserveerd)
                                 {
-                                    if (tijdEnDatumNaarDatum(data) == checkDatum) //Als het op dezelfde dag plaats vind
+                                    if(isEenTafelAlGereserveerd(data, checkDatum, checkTijd))
                                     {
-                                        if (tijdCheck(checkTijd, tijdEnDatumNaarInt(data), 200)) //checkt of de tafel al gereserveerd is ergens binnen 200 (2 uur) van tevoren of 2 uur daarna, zo ja, Is al gereserveerd.
-                                            alGereserveerd = true;
+                                        alGereserveerd = true;
+                                        break;
                                     }
                                 }
                                 if (plekkenDieNogMoetenWordenGekoppeld <= 2 && tafel.Plekken != 2 && !alGereserveerd) //Als er nog maar 2 of minder mensen zijn die een tafel nodig heeft van een reservering, kijkt het systeem om de tafel 2 personen heeft om ruimte te besparen
@@ -153,49 +134,49 @@ namespace TheFatDuckRestaurant
                                     return gereserveerdeTafels;
                             }
                         }
-                            SaveTafels(this);
-                            Console.WriteLine("Toets op een knop om verder te gaan");
-                            Console.ReadLine();
-                            return gereserveerdeTafels;
-                       case '0':
-                           return gereserveerdeTafels;
-                       default:
-                           wrongInput = true;
-                           break;
-                   }
-               }
-           }
-         
-        
+                        SaveTafels(this);
+                        Console.WriteLine("Toets op een knop om verder te gaan");
+                        Console.ReadLine();
+                        return gereserveerdeTafels;
+                    case '0':
+                        return gereserveerdeTafels;
+                    default:
+                        wrongInput = true;
+                        break;
+                }
+            }
+        }
+
+
         public List<Tafel> KoppelenDoorMedewerker(int aantalMensen, List<Tafel> gereserveerdeTafels, string tijdEnDatum) //Koppelen door medewerker heeft als input een int aantal mensen, en list van tafels die al gekoppeld zijn aan de reservering.
         {
             int checkTijd = tijdEnDatumNaarInt(tijdEnDatum); //De gegeven tijd in int (1200)
             string checkDatum = tijdEnDatumNaarDatum(tijdEnDatum); //De gegeven datum in string (Zaterdag 1 januari 2021)
             while (true)
-        {
-           Console.Clear();
-           Console.WriteLine(ASCIIART.TafelsArt());
-           if (BerekenTafelsDieNogGekoppeldMoetenWorden(aantalMensen, gereserveerdeTafels) <= 0) //0 megenomen omdat dat aangeeft dat er precies genoeg tafels zijn gekoppeld.
-           {
-               Console.WriteLine("Er zijn genoeg tafels gekoppeld aan deze reservering. Klik op een toets om terug te gaan");
-               SaveTafels(this);
-               ConsoleKeyInfo input = Console.ReadKey();
-               return gereserveerdeTafels;
-           }
-           Console.WriteLine("Voer het ID van een tafel in om hem toe te voegen aan de reservering (bijv: 1A)\n\n0: Terug");
-           string userInput = Console.ReadLine();
-           if(userInput == "0")
-           {
-               SaveTafels(this);
-               return gereserveerdeTafels;
-           }
+            {
+                Console.Clear();
+                Console.WriteLine(ASCIIART.TafelsArt());
+                if (BerekenTafelsDieNogGekoppeldMoetenWorden(aantalMensen, gereserveerdeTafels) <= 0) //0 megenomen omdat dat aangeeft dat er precies genoeg tafels zijn gekoppeld.
+                {
+                    Console.WriteLine("Er zijn genoeg tafels gekoppeld aan deze reservering. Klik op een toets om terug te gaan");
+                    SaveTafels(this);
+                    ConsoleKeyInfo input = Console.ReadKey();
+                    return gereserveerdeTafels;
+                }
+                Console.WriteLine("Voer het ID van een tafel in om hem toe te voegen aan de reservering (bijv: 1A)\n\n0: Terug");
+                string userInput = Console.ReadLine();
+                if (userInput == "0")
+                {
+                    SaveTafels(this);
+                    return gereserveerdeTafels;
+                }
                 bool succesvolGekoppeld = false;
                 foreach (Tafel tafel in Tafels)
-           {
-                bool alGereserveerd = false;
-                if (tafel.Gereserveerd == null)
-                    tafel.Gereserveerd = new List<string>();
-                if(userInput == tafel.ID)
+                {
+                    bool alGereserveerd = false;
+                    if (tafel.Gereserveerd == null)
+                        tafel.Gereserveerd = new List<string>();
+                    if (userInput == tafel.ID)
                     {
                         foreach (string data in tafel.Gereserveerd)
                         {
@@ -205,18 +186,18 @@ namespace TheFatDuckRestaurant
                                     alGereserveerd = true;
                             }
                         }
-                            if (!alGereserveerd)
-                            {
-                                Console.Clear();
-                                Console.WriteLine(ASCIIART.TafelsArt());
-                                gereserveerdeTafels.Add(tafel);
-                                tafel.Gereserveerd.Add($"{tijdEnDatumNaarInt(tijdEnDatum)}{tijdEnDatumNaarDatum(tijdEnDatum)}"); //1200Zaterdag 1 januari 2021
-                                succesvolGekoppeld = true;  
-                                Console.WriteLine($"{tafel.ID} toegevoegd aan de reservering!\n\nKlik op een toets en klik enter om verder te gaan");
-                                Console.ReadLine();
-                                break;
-                            }
+                        if (!alGereserveerd)
+                        {
+                            Console.Clear();
+                            Console.WriteLine(ASCIIART.TafelsArt());
+                            gereserveerdeTafels.Add(tafel);
+                            tafel.Gereserveerd.Add($"{tijdEnDatumNaarInt(tijdEnDatum)}{tijdEnDatumNaarDatum(tijdEnDatum)}"); //1200Zaterdag 1 januari 2021
+                            succesvolGekoppeld = true;
+                            Console.WriteLine($"{tafel.ID} toegevoegd aan de reservering!\n\nKlik op een toets en klik enter om verder te gaan");
+                            Console.ReadLine();
+                            break;
                         }
+                    }
                 }
                 if (!succesvolGekoppeld)
                 {
@@ -226,25 +207,24 @@ namespace TheFatDuckRestaurant
                     Console.ReadLine();
                 }
             }
+        }
+
+        public void SaveTafels(TafelArray tafels)
+        {
+            var JSONoptions = new JsonSerializerOptions
+            {
+                WriteIndented = true,
+            };
+            File.WriteAllText("Tafels.json", JsonSerializer.Serialize(tafels, JSONoptions));
+        }
     }
 
-    public void SaveTafels(TafelArray tafels)
-{
-var JSONoptions = new JsonSerializerOptions
-{
-   WriteIndented = true,
-};
-File.WriteAllText("Tafels.json", JsonSerializer.Serialize(tafels, JSONoptions));
-}
-}
+    public class Tafel
+    {
+        public string ID { get; set; }
+        public int Plekken { get; set; }
+        public List<string> Gereserveerd { get; set; }
 
-
-public class Tafel
-{
-public string ID { get; set; }
-public int Plekken { get; set; }
-public List<string> Gereserveerd { get; set; }
-
-public Tafel() { } //Empty constructor for json desrializen
-}
+        public Tafel() { } //Empty constructor for json desrializen
+    }
 }
